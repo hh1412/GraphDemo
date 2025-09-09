73eb4b4f63db026d23fee3e7ccfea1777b340fa0
huanghao1412 <huanghao1412@163.com>
2025-09-10 00:38:18 +0800

    加涟漪和 蚂蚁线动画

--- src/views/GraphDemo/index.vue
+++ src/views/GraphDemo/index.vue
@@ -5,7 +5,33 @@
 </template>
 
 <script setup lang="ts">
-import { Graph } from '@antv/g6';
+import { Circle, ExtensionCategory, Graph, Line, register } from '@antv/g6';
+
+class BreathingCircle extends Circle {
+    onCreate() {
+        const halo = this.shapeMap.halo;
+        halo.animate([{ lineWidth: 0 }, { lineWidth: 20 }], {
+            duration: 1000,
+            iterations: Infinity,
+            direction: 'alternate',
+        });
+    }
+}
+
+register(ExtensionCategory.NODE, 'breathing-circle', BreathingCircle);
+
+class AntLine extends Line {
+    onCreate() {
+        const shape = this.shapeMap.key;
+        shape.animate([{ lineDashOffset: -20 }, { lineDashOffset: 0 }], {
+            duration: 500,
+            iterations: Infinity,
+        });
+    }
+}
+
+register(ExtensionCategory.EDGE, 'ant-line', AntLine);
+
 
 fetch('https://assets.antv.antgroup.com/g6/cluster.json')
     .then((res) => res.json())
@@ -14,13 +40,22 @@ fetch('https://assets.antv.antgroup.com/g6/cluster.json')
             container: 'container',
             data,
             node: {
+                type: 'breathing-circle',
                 style: {
+                    size: 50,
+                    halo: true,
                     labelText: (d) => d.id,
                     ports: [],
                 },
                 palette: {
                     type: 'group',
                     field: 'cluster',
+                },
+            },
+            edge: {
+                type: 'ant-line',
+                style: {
+                    lineDash: [10, 10],
                 },
             },
             layout: {
